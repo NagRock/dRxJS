290c79c79c6a808e32d884707e98c266a2c253fb
Kamil.Knitter <kamil.knitter@dynatrace.com>
2019-11-03 20:49:25 +0100

    runNgZoneExample

--- demo-app/src/app/app.component.ts
+++ demo-app/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, NgZone} from '@angular/core';
 import * as examples from './examples';
+import {Observable} from 'rxjs';
+import {startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,40 @@ import * as examples from './examples';
 })
 export class AppComponent {
   examples = Object.entries(examples).map(([name, run]) => ({name, run}));
+
+  constructor(
+    private readonly ngZone: NgZone,
+  ) {
+  }
+
+  runNgZoneExample() {
+    const o$ = new Observable((o) => {
+      this.ngZone.runOutsideAngular(() => {
+        o.next(1);
+        o.next(2);
+        setTimeout(() => {
+          o.next(3);
+          o.complete();
+        });
+      });
+    }).pipe(
+      (observable) =>
+        new Observable((observer) =>
+          this.ngZone.runOutsideAngular(() => {
+            let sub;
+            const id = setTimeout(() => {
+              sub = observable.pipe(startWith(42)).subscribe(observer);
+            }, 1000);
+            return () => {
+              if (sub) {
+                sub.unsubscribe();
+              } else {
+                clearTimeout(id);
+              }
+            };
+          }))
+    );
+
+    o$.subscribe();
+  }
 }
