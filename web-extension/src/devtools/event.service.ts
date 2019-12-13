278fa104a6d4285ecadf8f8d0fcb593e13cf853d
Kamil.Knitter <kamil.knitter@dynatrace.com>
2019-12-13 14:52:55 +0100

    switchMap -> concatMap !

--- web-extension/src/devtools/event.service.ts
+++ web-extension/src/devtools/event.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
-import {from, interval, ReplaySubject} from "rxjs";
-import {filter, map, switchMap, tap} from 'rxjs/operators';
+import {from, interval, of, ReplaySubject, throwError} from 'rxjs';
+import {concatMap, filter, map, switchMap, tap} from 'rxjs/operators';
 import {browser} from "../types/webextension-polyfill-ts";
 import {Event} from '@drxjs/events';
 
@@ -15,7 +15,7 @@ export class EventService {
     interval(1000)
       .pipe(
         tap(() => console.log('call flush')),
-        switchMap(() => from(browser.devtools.inspectedWindow.eval(this.flushFuncName))),
+        concatMap(() => from(browser.devtools.inspectedWindow.eval(this.flushFuncName))),
         map(data => data[0]),
         filter(events => events && events.length !== 0),
       )
